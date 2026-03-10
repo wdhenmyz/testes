9f75956bf0e3229b424e7341669421452de21da6
wesllen dhenmyz brito ferreira <wesllen.dhenmyz@outlook.com>
2026-03-10 14:16:52 +0000

    facilitando exportação

---
+++ vanilla-js-kit/vanilla-kit.mjs
@@ -0,0 +1,5 @@
+import Element from "./Element.mjs";
+import DOM from "./DOM.mjs";
+import Template from "./Template.mjs";
+import Store from "./Store.mjs";
+import asyncStore from "./asyncStore.mjs";
