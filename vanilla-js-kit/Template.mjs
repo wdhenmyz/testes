4c59cb20895e6c6a4430d2d049eb778ed5e97108
wesllen dhenmyz brito ferreira <wdbf2003@gmail.com>
2026-03-23 16:27:30 -0300

    Atualizar o Template.mjs

--- vanilla-js-kit/Template.mjs
+++ vanilla-js-kit/Template.mjs
@@ -1,33 +1,56 @@
 import DOM from './DOM.mjs'
 
 export default class Template {
-    constructor(type, selector) {
-        // carregar os elementos
-        this.dom = new DOM(type, selector)
-
-        // definir e renderizar o template
+    constructor() {
         this.stores = this.Store();
-        this._applyAttributes();
-        this._render();
+        this.attributes = this.ATRIBUTTES();
+        this.classes = this.class();
+        this.functions = this.FUNCTIONS();
     }
 
-    HTML() {
+    HTML () {
         return ``
     }
 
-    CSS() {
+    Style () {
+        return``
+    }
+
+    Script () {
+        return``
+    }
+
+    ATRIBUTTES () {
         return {}
     }
 
-    ATRIBUTTES() {
+    class () {
         return {}
     }
-
-    FUNCTIONS(dom) {}
 
     Store () {
         return {}
     }
+
+    FUNCTIONS () {
+        return {}
+    }
+
+    Render() {
+        const el = document.querySelector('body')
+
+        el.innerHTML = this.HTML()
+
+        const style = document.createElement("style")
+        style.textContent = this.Style()
+        document.head.appendChild(style)
+
+        const script = document.createElement("script")
+        script.textContent = this.Script()
+        document.body.appendChild(script)
+    }
+
+
 
     _render() {
         this.dom.innerHTML(this.HTML());
